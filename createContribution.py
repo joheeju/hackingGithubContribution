c19ed96916bba55ee4aa24beb847368a51034450
Heejoo Cho <joheeju@gatech.edu>
2024-05-08 10:19:04 -0700

    Fake commit

--- createContribution.py
+++ createContribution.py
@@ -1,5 +1,11 @@
 import subprocess
 
+
 if __name__ == '__main__':
     for i in range(1, 400):
-
+        with open("fakeContribution.txt", "a") as f:
+            f.write("try ")
+        subprocess.run(["git", "add", "."])
+        subprocess.run(["git", "commit", "--date", f"\"{i} day ago\"", "-m", "Fake commit"])
+        subprocess.run(["git", "push"])
+        print(f"Commit pushed {i}")
